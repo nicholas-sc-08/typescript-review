cc2755053cbc4bb5a5acf19e165631a02d90bb0b
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-08-21 11:45:55 -0300

    feat: initial project

---
+++ fetchjson/index.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then(res => {
+    const todo = res.data as Todo;
+
+    const { id, title, completed } = todo;
+
+    console.log(`
+        The Todo with ID: ${id}
+        Has a Title of: ${title}
+        Is it finished? ${completed}
+        `);
+
+});
