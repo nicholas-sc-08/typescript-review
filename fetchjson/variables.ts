9bfe8777f8855e1cec7e5d9c9b0cc41c5d2733a3
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-09-10 09:37:33 -0300

    feat: type annotations

---
+++ fetchjson/variables.ts
@@ -0,0 +1,23 @@
+const apples: number = 90;
+const now: Date = new Date();
+let colors: string[] = ['red', 'green'];
+let truths: boolean[] = [true, false, true];
+
+class Car {
+    asd(){}
+}
+
+let car: Car = new Car();
+car.asd()
+
+interface Point {
+    x: number;
+    y: number;
+    z: number;
+}
+
+let point = {
+    x: 12,
+    y: -8,
+    z: 1
+}
