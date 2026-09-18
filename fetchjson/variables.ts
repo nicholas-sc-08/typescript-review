a9405d2ae6f064d10cb7334a2bfcf4d20273b383
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-09-18 10:26:07 -0300

    feat: uderstanding super

--- fetchjson/variables.ts
+++ fetchjson/variables.ts
@@ -3,21 +3,43 @@ const now: Date = new Date();
 let colors: string[] = ['red', 'green'];
 let truths: boolean[] = [true, false, true];
 
-class Car {
-    asd(){}
-}
-
-let car: Car = new Car();
-car.asd()
-
 interface Point {
     x: number;
     y: number;
     z: number;
 }
 
+type Functions = (i: number) => void;
+
 let point = {
     x: 12,
     y: -8,
     z: 1
+}
+
+const logNumber: Functions = (i: number) => {
+    console.log(i);
+}
+
+//whem to use annotations
+//1. function that returns the 'any' type
+const json = '{x: 10, y: 20'
+const coordinates = JSON.parse(json);
+console.log(coordinates);
+
+let words = ['asad',  'adasdasd'];
+let foundWord;
+
+for(let i = 0; i < words.length; i++) {
+    
+}
+
+class Vehicle  {
+    constructor(protected color: string, protected model: string) {}
+}
+
+class Car extends Vehicle {
+    constructor(color: string) {
+        super('asd', 'sasd');
+    }
 }
